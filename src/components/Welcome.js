e0ebd374933bb1f9987c0bda98c86cb9a6d0d23a
Tribhuvan Sharma <ds14041998@gmail.com>
2020-11-09 14:23:27 +0530

    Props

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -4,7 +4,7 @@ import React from "react";
 function Welcome(props){
     return (
         <div>
-            "Hey !" <h1>{props.name}.</h1>
+            <h1>"Hey !"{props.name}.</h1>
             <h2>"Welcome to Newton School."</h2>
         </div>
     )
